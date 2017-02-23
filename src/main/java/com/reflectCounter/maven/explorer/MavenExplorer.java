460e79a5704b35c7405815daf51bea083ae2199b
jcarlos <carlosmaster10@gmail.com>
2017-02-23 13:58:03 -0300

    MavenExplorer created

---
+++ src/main/java/com/reflectCounter/maven/explorer/MavenExplorer.java
@@ -0,0 +1,55 @@
+package com.reflectCounter.maven.explorer;
+
+import java.io.File;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class MavenExplorer {
+
+	private File pom = null;
+
+	public MavenExplorer(File pom) {
+		this.pom = pom;
+	}
+	
+	private String getXmlInfo(String elementName) throws Exception {
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder builder = factory.newDocumentBuilder();
+		
+		Document doc = builder.parse(this.pom);
+		doc.getDocumentElement().normalize();
+		
+		NodeList nList = doc.getElementsByTagName(elementName);
+		
+		Node nNode = nList.item(0);
+		if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+			Element eElement = (Element) nNode;
+			return eElement.getTextContent().trim();
+		}
+		
+		return null;
+	}
+
+	public String getGroup() {
+		try {
+			return this.getXmlInfo("groupId");
+		} catch (Exception e) {
+			return null;
+		}
+	}
+
+	public String getArtifact() {
+		try {
+			return this.getXmlInfo("artifactId");
+		} catch (Exception e) {
+			return null;
+		}
+	}
+
+}
