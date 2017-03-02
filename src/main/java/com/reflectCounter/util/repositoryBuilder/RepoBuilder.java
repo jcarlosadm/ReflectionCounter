ba5aa4f343393a18f96766fbd3e1a599ba19529e
jcarlos <carlosmaster10@gmail.com>
2017-03-02 17:10:26 -0300

    change abstract class to interface

--- src/main/java/com/reflectCounter/util/repositoryBuilder/RepoBuilder.java
+++ src/main/java/com/reflectCounter/util/repositoryBuilder/RepoBuilder.java
@@ -2,18 +2,10 @@ package com.reflectCounter.util.repositoryBuilder;
 
 import java.io.File;
 
-public abstract class RepoBuilder {
+public interface RepoBuilder {
 
-	protected String projectFolder = "";
+	public boolean build();
 	
-	public RepoBuilder(String projectFolder) {
-		// TODO Auto-generated constructor stub
-		this.projectFolder = projectFolder;
-		
-	}
-	
-	public abstract boolean build();
-	
-	public abstract File getJar();
+	public File getJar();
 	
 }
