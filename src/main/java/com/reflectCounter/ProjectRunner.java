6b0c065c4ae6617a47afad13af37a5429d76a785
jcarlos <carlosmaster10@gmail.com>
2017-03-02 17:12:14 -0300

    ProjectRunner finished

--- src/main/java/com/reflectCounter/ProjectRunner.java
+++ src/main/java/com/reflectCounter/ProjectRunner.java
@@ -1,119 +1,148 @@
 package com.reflectCounter;
 
 import java.io.File;
-import java.util.List;
+
+import org.apache.commons.io.FileUtils;
 
 import com.reflectCounter.asm.AsmRunner;
-import com.reflectCounter.exploreLocalRepo.ExplorerLocalRepository;
-import com.reflectCounter.exploreLocalRepo.ExplorerMode;
-import com.reflectCounter.github.api.FileFinder;
-import com.reflectCounter.github.api.SearchTerms;
-import com.reflectCounter.gradle.builder.GradleBuilder;
 import com.reflectCounter.maven.api.MavenProject;
-import com.reflectCounter.maven.builder.MavenBuilder;
-import com.reflectCounter.util.CSVBuilder;
+import com.reflectCounter.util.ErrorReport;
+import com.reflectCounter.util.OcurrencesCsv;
 import com.reflectCounter.util.Repository;
 import com.reflectCounter.util.repositoryBuilder.RepoBuilder;
 
 public class ProjectRunner {
 
-	private String projectName = "";
+//	private String projectName = "";
 
 	private Repository repository = null;
 
-	private CSVBuilder csvBuilder = null;
+	private OcurrencesCsv csvBuilder = null;
 
-	public ProjectRunner(String url, CSVBuilder csvBuilder) {
+	public ProjectRunner(String url, OcurrencesCsv csvBuilder) {
 		this.repository = new Repository(url);
-		this.projectName = this.repository.getRepoFolderName()
-				.substring(this.repository.getRepoFolderName().lastIndexOf(File.separator));
+//		this.projectName = this.repository.getRepoFolderName()
+//				.substring(this.repository.getRepoFolderName().lastIndexOf(File.separator));
 		this.csvBuilder = csvBuilder;
+		this.csvBuilder.setProjectName(this.repository.getUrlProject());
 	}
 
 	public void run() throws Exception {
-		// TODO search artifacts if is maven project
+		
+		System.out.println(System.lineSeparator());
+		
+		System.out.println("cloning " + this.repository.getUrlProject());
+		if (!this.repository.cloneRepo()) {
+			ErrorReport.getInstance().write("error to clone "+ this.repository.getUrlProject());
+			System.out.println("error to clone repository. exiting");
+			this.deleteProjectFolder();
+			return;
+		}
+		
+		// TODO remove if uses github api
+		if (!this.repository.isMavenProject() && !this.repository.isGradleProject()) {
+			System.out.println("this is not a gradle or maven repository. exiting");
+			this.deleteProjectFolder();
+			return;
+		}
+		
+		// search artifacts if is maven project
+		
 		if (this.repository.isMavenProject()) {
 			MavenProject mavenProject = new MavenProject(this.getPomFile());
+			System.out.println("this is a maven project. searching public artifacts");
 			String jarPath = mavenProject.downloadJar();
 			if (jarPath != null && !jarPath.isEmpty()) {
-				// TODO run App with jar and exits
-				// TODO create enum to methods to explore
-				System.out.println("run App in jar");
-				//AsmRunner asmRunner = new AsmRunner(jarPath, targetClass, targetMethodDeclaration)
+				this.runAsmTool(jarPath);
+				this.deleteProjectFolder();
 				return;
-			}
+			} else
+				System.out.println("error to download jar");
 		}
-
-		SearchTerms searchTerms = this.fillBasicSearchTerms();
-
-		List<String> listReflectFiles = null;
-		this.fillListOfFiles(searchTerms, listReflectFiles, "import java.lang.reflect");
-
-		List<String> listUnsafeFiles = null;
-		this.fillListOfFiles(searchTerms, listUnsafeFiles, "import sun.misc.Unsafe");
-
-		if (!this.repository.cloneRepo()) {
-			throw new Exception("error to clone repository " + this.repository.getUrlProject());
+		
+		// build
+		
+		RepoBuilder repoBuilder = this.repository.getRepoBuilderInstance();
+		System.out.println("building project " + this.repository.getUrlProject());
+		if (repoBuilder.build() == false) {
+			ErrorReport.getInstance().write("error to build "+this.repository.getUrlProject());
+			System.out.println("error to build this project");
+			this.deleteProjectFolder();
+			return;
 		}
-
-		System.out.println("Project: " + this.projectName);
-
-		ExplorerLocalRepository expLocalRepo = new ExplorerLocalRepository(
-				new File(this.repository.getRepoFolderName()), this.csvBuilder);
-
-		List<String> suspiciousReflectList = expLocalRepo.explore(listReflectFiles, ExplorerMode.REFLECTION);
-		List<String> suspiciousUnsafeList = expLocalRepo.explore(listUnsafeFiles, ExplorerMode.UNSAFE);
-
-		// TODO maybe remove
-		RepoBuilder repoBuilder = null;
-		if (repository.isMavenProject()) {
-			repoBuilder = new MavenBuilder(this.repository.getRepoFolderName());
-		} else if (repository.isGradleProject()) {
-			repoBuilder = new GradleBuilder(this.repository.getRepoFolderName());
-		} else {
-			throw new Exception(this.projectName + " is not a maven or gradle project. abort");
+		
+		File jar = repoBuilder.getJar();
+		if (jar == null || !jar.exists() || jar.isDirectory()) {
+			ErrorReport.getInstance().write("error to build "+this.repository.getUrlProject());
+			System.out.println("jar don't exists");
+			this.deleteProjectFolder();
+			return;
 		}
+		
+		this.runAsmTool(jar.getAbsolutePath());
+		
+		this.deleteProjectFolder();
 
-		if (!repoBuilder.build()) {
-			throw new Exception("error to build project " + this.projectName);
-		}
+//		// using github api
+//		
+//		SearchTerms searchTerms = this.fillBasicSearchTerms();
+//
+//		List<String> listReflectFiles = null;
+//		this.fillListOfFiles(searchTerms, listReflectFiles, "import java.lang.reflect");
+//
+//		List<String> listUnsafeFiles = null;
+//		this.fillListOfFiles(searchTerms, listUnsafeFiles, "import sun.misc.Unsafe");
+//
+//		if (!this.repository.cloneRepo()) {
+//			throw new Exception("error to clone repository " + this.repository.getUrlProject());
+//		}
+//
+//		System.out.println("Project: " + this.projectName);
+//
+//		ExplorerLocalRepository expLocalRepo = new ExplorerLocalRepository(
+//				new File(this.repository.getRepoFolderName()), this.csvBuilder);
+//
+//		List<String> suspiciousReflectList = expLocalRepo.explore(listReflectFiles, ExplorerMode.REFLECTION);
+//		List<String> suspiciousUnsafeList = expLocalRepo.explore(listUnsafeFiles, ExplorerMode.UNSAFE);
+//
+	}
 
-		File jarFile = repoBuilder.getJar();
+	private void deleteProjectFolder() throws Exception {
+		FileUtils.deleteDirectory(new File(this.repository.getRepoFolderName()));
+	}
 
-		//AsmRunner app = new AsmRunner();
-
-		/*
-		 * TODO call app over methods of classes of reflection api, and over
-		 * methods of Unsafe class write frequency of the method in csv file
-		 */
-		// app.findCallingMethodsInJar(jarPath, targetClass, targetMethod);
+	private void runAsmTool(String jarPath) {
+		System.out.println("running asm tool in " + jarPath);
+		AsmRunner asmRunner = new AsmRunner(jarPath, this.csvBuilder);
+		asmRunner.run();
+		System.out.println("finished");
 	}
 
 	private File getPomFile() {
 		return new File(this.repository.getRepoFolderName() + File.separator + "pom.xml");
 	}
 
-	private SearchTerms fillBasicSearchTerms() {
-		SearchTerms searchTerms = new SearchTerms();
-		searchTerms.setAuthorname(this.getAuthorName());
-		searchTerms.setLanguage("java");
-		searchTerms.setReponame(this.projectName);
-		return searchTerms;
-	}
-
-	private void fillListOfFiles(SearchTerms searchTerms, List<String> list, String mainSearchTerm) {
-		searchTerms.setMainSearchTerm(mainSearchTerm);
-		try {
-			FileFinder fileFinderReflect = new FileFinder(searchTerms);
-			list = fileFinderReflect.run();
-		} catch (Exception e) {
-			list = null;
-		}
-	}
-
-	private String getAuthorName() {
-		String url = this.repository.getUrlProject();
-		url = url.substring(0, url.lastIndexOf("/"));
-		return url.substring(url.lastIndexOf("/") + 1);
-	}
+//	private SearchTerms fillBasicSearchTerms() {
+//		SearchTerms searchTerms = new SearchTerms();
+//		searchTerms.setAuthorname(this.getAuthorName());
+//		searchTerms.setLanguage("java");
+//		searchTerms.setReponame(this.projectName);
+//		return searchTerms;
+//	}
+//
+//	private void fillListOfFiles(SearchTerms searchTerms, List<String> list, String mainSearchTerm) {
+//		searchTerms.setMainSearchTerm(mainSearchTerm);
+//		try {
+//			FileFinder fileFinderReflect = new FileFinder(searchTerms);
+//			list = fileFinderReflect.run();
+//		} catch (Exception e) {
+//			list = null;
+//		}
+//	}
+//
+//	private String getAuthorName() {
+//		String url = this.repository.getUrlProject();
+//		url = url.substring(0, url.lastIndexOf("/"));
+//		return url.substring(url.lastIndexOf("/") + 1);
+//	}
 }
